74580be2ec885bd4a2ea8203e2e59f856e093d7e
chamithjeewantha <chamithjeewa123@gmail.com>
2025-03-16 12:54:53 +0530

    Reverse the left and right numbers like
    You must use only a loop
    
    input  9
    
    output 4 3 2 1 5 9 8 7 6

--- Q5.java
+++ Q5.java
@@ -4,5 +4,18 @@ public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+        reverse(n);
+    }
+        public static void reverse(int n) {
+            int mid = n / 2;
+            for (int i = mid; i >= 1; i--) {
+                System.out.print(i + " ");
+            }
+            for (int i = n; i > mid; i--) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+    
     }
 }
